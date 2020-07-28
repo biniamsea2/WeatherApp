1634b91cf016173d6514fef4d60e1e2d3a1f0ce9
Biniam <Biniamt23@yahoo.com>
2020-07-28 00:19:31 -0700

    reading json file correctly

--- WeatherApplication/WeatherApplication/Models/WeatherForecastModel.cs
+++ WeatherApplication/WeatherApplication/Models/WeatherForecastModel.cs
@@ -14,13 +14,4 @@ namespace WeatherApplication.Models
         public string Timezone { get; set; }
     }
 
-    public class DayForecastModel
-    {
-        public string weather_state_name { get; set; }
-        public string applicable_date { get; set; }
-        public float min_temp { get; set; }
-        public float max_temp { get; set; }
-        public float the_temp { get; set; }
-    }
-
 }
