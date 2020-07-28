5437981f33b76e8d1fbb5728b4938d5da076755c
Biniam <Biniamt23@yahoo.com>
2020-07-27 22:48:13 -0700

    classes created

---
+++ WeatherApplication/WeatherApplication/Models/WeatherForecastModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherApplication.Models
+{
+    public class WeatherForecastModel
+    {
+        public DayForecastModel[] consolidated_weather { get; set; }
+        public DateTime Sun_rise { get; set; }
+        public DateTime Sun_set { get; set; }
+        public string Title { get; set; }
+        public string Timezone { get; set; }
+    }
+
+    public class DayForecastModel
+    {
+        public string weather_state_name { get; set; }
+        public string applicable_date { get; set; }
+        public float min_temp { get; set; }
+        public float max_temp { get; set; }
+        public float the_temp { get; set; }
+    }
+
+}
